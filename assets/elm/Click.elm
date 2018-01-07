809fe86e47bea8b2195de8b6528b885edebac7aa
Andy <Andy>
2018-01-07 21:42:42 +0800

    Layout click page

--- assets/elm/Click.elm
+++ assets/elm/Click.elm
@@ -5,6 +5,10 @@ import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import String
 
+import Svg
+import Svg.Events as SE
+import Svg.Attributes as SA
+
 
 main =
     programWithFlags
@@ -58,7 +62,52 @@ subscriptions model = online Online
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
-    div [] [div [] [text model.name, text "|", text <| toString model.click_count, text "|", text <| toString model.online_guests], div [] [img [ src "/images/heart.png", onClick Click] []] ]
+    div [ class "is-unselectable"]
+        [ section [class "hero"]
+              [ div [class "hero-body"]
+                    [ div [class "container"]
+                          [ heart
+                          ]
+                    ]
+              ]
+        , count model
+        , info model
+        ]
+        
+info : Model -> Html Msg
+info model =
+    section [class "section is-medium"]
+        [
+         div [class "container"]
+                  [ div [class "level is-mobile"]
+                        [ span [ class "level-item"] [ span [ class "subtitle"] [text "在線賓客"] ]
+                        , span [ class "level-item"] [span [ class "tag is-danger"] [ text <| toString model.online_guests ]]
+                        ]
+                  ]
+
+        ]
+
+count : Model -> Html Msg
+count model =
+    section [class "section is-medium"]
+        [ div [ class "container"]
+              [div [ class "level is-mobile" ]
+                   [ div [class "level-item"] [ span [ class "subtitle"] [text "送上祝福"]]
+                   , div [class "level-item"] [ span [ class "tag is-large is-danger"] [text <| toString model.click_count ]]]
+                  ]
+        ]
+
+
+
+heart : Svg.Svg Msg
+heart = Svg.svg [SA.viewBox "0 0 100 100", SE.onClick Click]
+    [ Svg.g []
+        [ Svg.path [ SA.class "st0", SA.d "M44.6,58.362c-3.366-14.711,14.057-23.423,22.008-10.44c3.9-6.369,10.077-7.508,14.964-5.288 c2.301-21.225-23.2-32.831-35.14-13.331C33.919,8.87,6.498,22.58,11.796,45.733C16.978,68.389,46.433,80.37,46.433,80.37 s4.608-1.88,10.494-5.543C51.812,71.123,46.256,65.601,44.6,58.362z" ]
+            []
+        , Svg.g []
+            [ Svg.path [ SA.class "st0", SA.d "M66.69,47.921c7.95-12.983,25.375-4.271,22.008,10.44C85.404,72.757,66.69,80.37,66.69,80.37 s-18.715-7.612-22.008-22.008C41.316,43.65,58.739,34.939,66.69,47.921z" ] []
+            ]
+        ]
+    ]
