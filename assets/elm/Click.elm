2a39ef966edd0ca8959f25d7b2bd5a29865ec090
Andy <Andy>
2018-01-08 00:01:24 +0800

    Add small hearts

--- assets/elm/Click.elm
+++ assets/elm/Click.elm
@@ -7,6 +7,8 @@ import String
 import Svg
 import Svg.Events as SE
 import Svg.Attributes as SA
+import Time as T
+import Random 
 
 
 main =
@@ -21,6 +23,7 @@ main =
 
 -- MODEL
 
+type alias Heart = { x : Int, dur : Int }
 
 type alias Flags =
     { name : String
@@ -32,12 +35,13 @@ type alias Model =
     { name : String
     , click_count : Int
     , online_guests : Int
+    , hearts : List Heart
     }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( Model flags.name flags.clicks 0, Cmd.none )
+    ( Model flags.name flags.clicks 0 [], Cmd.none )
 
 
 
@@ -50,16 +54,26 @@ port click : Int -> Cmd msg
 type Msg
     = Click
     | Online Int
+    | Tick T.Time
+    | NewHeart Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Click ->
-            ( { model | click_count = model.click_count + 1 }, click 1 )
+            ( { model | click_count = model.click_count + 1 }, Cmd.batch [click 1, Random.generate NewHeart (Random.int 0 1000)] )
 
         Online c ->
             ( { model | online_guests = c }, Cmd.none )
+
+        Tick _ ->
+            let past heart = { heart | dur = heart.dur + 1}
+                isDead heart = heart.dur < 30
+            in ({model | hearts = List.filter isDead <| List.map past model.hearts}, Cmd.none)
+
+        NewHeart x ->
+            ({model | hearts = model.hearts ++ [Heart x 0]}, Cmd.none)
 
 
 port online : (Int -> msg) -> Sub msg
@@ -67,8 +81,7 @@ port online : (Int -> msg) -> Sub msg
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    online Online
-
+    Sub.batch [online Online, T.every (T.second) Tick]
 
 
 -- VIEW
@@ -80,7 +93,7 @@ view model =
         [ section [ class "hero" ]
             [ div [ class "hero-body" ]
                 [ div [ class "container" ]
-                    [ heart
+                    [ hearts model.hearts
                     ]
                 ]
             ]
@@ -115,3 +128,16 @@ heart =
     Svg.svg [ SA.viewBox "0 0 100 100", SE.onClick Click ]
         [ Svg.path [ SA.class "st0", SA.d "M50.22,27.358c13.786-22.514,44.004-7.407,38.166,18.105C82.672,70.427,50.22,83.629,50.22,83.629 S17.764,70.427,12.054,45.463C6.216,19.952,36.431,4.844,50.22,27.358z" ] []
         ]
+
+smallHeart : Heart -> Svg.Svg Msg
+smallHeart heart =
+    Svg.svg [ SA.x <| toString heart.x, SA.y "1100", SA.opacity "1", SA.viewBox "0 0 1000 1000", SE.onClick Click ]
+        [ Svg.animate [ SA.attributeName "y", SA.to "-100", SA.dur "5s", SA.repeatCount "1"] []
+        , Svg.animate [ SA.attributeName "opacity", SA.to "0", SA.begin "2s", SA.dur "5s", SA.repeatCount "1"] []
+        , Svg.path [ SA.class "st0", SA.d "M50.22,27.358c13.786-22.514,44.004-7.407,38.166,18.105C82.672,70.427,50.22,83.629,50.22,83.629 S17.764,70.427,12.054,45.463C6.216,19.952,36.431,4.844,50.22,27.358z" ] []
+        ]
+
+
+hearts : List Heart -> Svg.Svg Msg
+hearts heartList =
+    Svg.svg [ SA.viewBox "0 0 1000 1000"] ([heart] ++ List.map smallHeart heartList)
